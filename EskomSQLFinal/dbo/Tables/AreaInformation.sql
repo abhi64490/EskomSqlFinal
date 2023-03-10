e6e55ec85685b8e3512a4d6119d1da59f4addd5c
abhi_ <abhi_@ABHI_AMD>
2023-03-10 13:22:20 +0530

    commit

---
+++ EskomSQLFinal/dbo/Tables/AreaInformation.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[area_info](
+	[EndDateTime] [varchar](60) NULL,
+	[Note] [varchar](60) NULL,
+	[StartEvent] [varchar](60) NULL,
+	[Name_info] [varchar](60) NULL,
+	[Region] [varchar](60) NULL,
+	[DaysName] [varchar](60) NULL,
+	[ScheduleDate] [varchar](60) NULL,
+	[stages] [varchar](60) NULL,
+	[areaid] [varchar](100) NULL,
+	[DateTimeUpdated] [datetime] NOT NULL
+) ON [PRIMARY]
+GO
