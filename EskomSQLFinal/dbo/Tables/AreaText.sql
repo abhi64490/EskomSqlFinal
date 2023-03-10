e35552fa414934f33c9dc1ce6205b02a887d7d91
abhi_ <abhi_@ABHI_AMD>
2023-03-10 13:17:36 +0530

    commit text

---
+++ EskomSQLFinal/dbo/Tables/AreaText.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AreaText](
+	[id] [varchar](100) NULL,
+	[name] [varchar](100) NULL,
+	[region] [varchar](100) NULL,
+	[text] [varchar](100) NULL,
+	[DateTimeUpdated] [datetime] NOT NULL
+) ON [PRIMARY]
+GO
