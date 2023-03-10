88f70053b0b5be7ffba9572f47334e87521b4441
abhi_ <abhi_@ABHI_AMD>
2023-03-10 12:36:49 +0530

    commit staged

---
+++ EskomSQLFinal/dbo/Tables/AreaGps.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[area_gps](
+	[area_id] [varchar](500) NULL,
+	[area_name] [varchar](500) NULL,
+	[region_name] [varchar](500) NULL,
+	[cordinates] [varchar](100) NULL,
+	[DateTimeUpdated] [datetime] NOT NULL
+) ON [PRIMARY]
+GO
