13579367bc1b6f010082766e82570127d864b0ef
abhi_ <abhi_@ABHI_AMD>
2023-03-10 16:20:07 +0530

    staged

---
+++ EskomSQLFinal/dbo/Tables/AreaScheduleGpsTxt.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[AreaScheduleGpsTxt](
+	[area_id] [varchar](500) NULL,
+	[area_name] [varchar](500) NULL,
+	[region_name] [varchar](500) NULL,
+	[date_val] [varchar](50) NULL,
+	[day_name] [varchar](50) NULL,
+	[start_time] [varchar](50) NULL,
+	[end_time] [varchar](50) NULL,
+	[DateTimeUpdated] [datetime] NOT NULL,
+	[Text] [varchar](100) NULL
+) ON [PRIMARY]
+GO
---
+++ EskomSQLFinal/dbo/Tables/TopicsNearby.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[topics_nearby](
+	[body] [varchar](500) NULL,
+	[category] [varchar](50) NULL,
+	[distance] [varchar](50) NULL,
+	[followers] [varchar](50) NULL,
+	[timestamps] [varchar](50) NULL
+) ON [PRIMARY]
+GO
